6da95edc18c7641fca0c5ec0c0619db719dc3a6e
xzz2021 <zipeng.2016@gmail.com>
2024-04-12 11:22:32 +0800

    更新vue报错,修复不刷新bug

--- content.js
+++ content.js
@@ -21,7 +21,6 @@ import platformObj from './src/pages/importPages'
 
 import { copy } from './src/api/directive'
 
-
   // 开发模式时  的  自动刷新
 if(DEBUG){   // 开发模式时为真   //   生产模式为假
   const { createWsConnect } = require('ws-reload-plugin')
--- webpack.watch.config.js
+++ webpack.watch.config.js
@@ -23,6 +23,7 @@ const watchconfig = {
           // 此处解决vue未定义extension大量报错问题
           __VUE_OPTIONS_API__: true,
           __VUE_PROD_DEVTOOLS__: false,
+          __VUE_PROD_HYDRATION_MISMATCH_DETAILS__ : false,
           "AUTHOR": JSON.stringify('xzz2022'),
           'DEBUG': true
         })
